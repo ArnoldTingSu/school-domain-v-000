902df303ad3854cb774d1374f16a7801e157fb75
Arnold Su <arnoldtingsu@gmail.com>
2018-11-27 21:43:11 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,20 @@
 # code here!
+
+
+class School
+  
+  
+  def initialize(roster)
+    @roster = {}
+  end
+  
+  def roster
+      @roster
+  end
+     
+def add_student(name, grade)
+  @roster[grade] = [name]
+ 
+  end
+
+end
