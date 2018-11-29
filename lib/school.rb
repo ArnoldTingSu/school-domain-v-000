ac14261fe68176231589133afe5ebcc57bbe0596
Arnold Su <arnoldtingsu@gmail.com>
2018-11-29 05:01:40 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,20 @@
 # code here!
+
+
+class School
+  
+  attr_reader :name, :roster
+  
+  
+  def initialize(name)
+    @name = name
+   @roster = {}
+  end
+  
+  
+  def add_student(name, grade)
+    grade[student]
+  end
+end
+ 
+     
